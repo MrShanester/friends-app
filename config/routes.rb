82e95dc5d6728f879cd04778595efc8ddcea8e13
Shane Nicholson <shanescottnicholson@gmail.com>
2022-01-04 14:17:24 -0800

    Adds routes for friend index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/friends" => "friends#index"
+  get "/friends/:id" => "friends#show"
+  post "/friends" => "friends#create"
+  patch "/friends/:id" => "friends#edit"
+  delete "/friends/:id" => "friends#destroy"
 end
