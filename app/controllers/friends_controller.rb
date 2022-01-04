712d20af6421876fc250ba8e4f6d30f9c5cd41e0
Shane Nicholson <shanescottnicholson@gmail.com>
2022-01-04 14:15:48 -0800

    Adds crud actions to friend controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,2 +1,31 @@
 class FriendsController < ApplicationController
+  def index
+    friend = Friend.all
+    render json: friend.as_json
+  end
+
+  def show
+    friend = Friend.find_by(id: params[:id])
+    render json: friend.as_json
+  end
+
+  def create
+    Friend.new(name: params[:name], friend_score: 0)
+    friend.save
+    render json: friend.as_json
+  end
+
+  def edit
+    friend = Friend.find_by(id: params[:id])
+    friend.name = params[:name] || friend.name
+    friend.friend_score = params[:friend_score] || friend.friend_score
+    friend.save
+    render json: friend.as_json
+  end
+
+  def destroy
+    friend = Friend.find_by(id: params[:id])
+    friend.delete
+    render json: {message: "Friend Cancelled!"}
+  end
 end
